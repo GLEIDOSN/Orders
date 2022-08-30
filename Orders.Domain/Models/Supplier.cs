da830a0e42342bf7e869cb6617bfd74ad8037837
Gleidson Freitas <fc_gleidson@hotmail.com>
2022-08-30 07:20:41 -0300

    Add Supplier Domain Model

---
+++ Orders.Domain/Models/Supplier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Domain.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string SocialReason { get; set; }
+        public string CNPJ { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string NameContact { get; set; }
+
+    }
+}
